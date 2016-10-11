e7a76815add7df2096d0307187647f8b55aea4cd
Bruno Emori <bruno.emori@gmail.com>
2016-10-11 11:13:11 -0300

    Constants file

---
+++ scripts/const.py
@@ -0,0 +1,17 @@
+#Simulation constants
+
+#---------------------------------
+
+#Coordinate constants
+X_COORD = 0
+Y_COORD = 1
+
+#Sensor constants
+FIRST_LASER = 0
+LAST_LASER = 360
+RANGE_MAX = 10 #Maximum reach of the robot's sensor
+RANGE_LIMIT = 0.6 #Verify!
+
+#Map constants
+MAP_WIDHT = 375 #Verify!
+MAP_HEIGHT = 200 #Verify!
