682aa1846dd96ee2521d52a761ccf9acc6067c9f
Bruno Emori <bruno.emori@gmail.com>
2016-10-13 15:46:41 -0300

    Simulation's map implementation

--- scripts/mapDef.py
+++ scripts/mapDef.py
@@ -1,6 +1,17 @@
-class simMap:
+class simCell:
     def __init__(self, occupancyGrid, qPheromone, rPath, visit):
         self.occupancyGrid = occupancyGrid
         self.qPheromone = qPheromone
         self.rPath = rPath
         self.visit = visit
+
+class globalMap:
+    def __init__(self, borderLeft, borderRight, borderSup, borderInf, cellMap):
+        self.borderLeft = borderLeft
+        self.borderRight = borderRight
+        self.borderSup = borderSup
+        self.borderInf = borderInf
+        self.cellMap = cellMap
+
+        
+
