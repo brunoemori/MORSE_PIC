3921b3ba848b0079da8212d441a6ddedc423fec1
brunoemori <bruno.emori@gmail.com>
2016-10-17 18:41:57 -0200

    Constants files

--- scripts/const.py
+++ scripts/const.py
@@ -5,6 +5,7 @@
 #Coordinate constants
 X_COORD = 0
 Y_COORD = 1
+Z_COORD = 2
 
 #Sensor constants
 FIRST_LASER = 0
@@ -13,5 +14,6 @@ RANGE_MAX = 10 #Maximum reach of the robot's sensor
 RANGE_LIMIT = 0.6 #Verify!
 
 #Map constants
-MAP_WIDHT = 375 #Verify!
-MAP_HEIGHT = 200 #Verify!
+MAP_WIDHT = 375
+MAP_HEIGHT = 200 
+RESL = 0.08 #Map resolution
