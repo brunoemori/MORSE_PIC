fda77cf8f9ad530a95137b8480d9ee010642408e
Bruno Emori <bruno.emori@gmail.com>
2016-11-30 17:01:18 -0200

    Fixed robot attribute to robot1

--- scripts/stopRobot.py
+++ scripts/stopRobot.py
@@ -2,7 +2,7 @@ from pymorse import Morse
 
 def main():
     with Morse() as morse:
-        motion = morse.robot1.motion
+        motion = morse.robot1.motion1
         v_w = {"v": 0, "w": 0}
         motion.publish(v_w)
         exit()
