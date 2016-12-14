6eb9c8b149c9a7eba091c6bbc34713c4103ea3fb
Bruno Emori <bruno.emori@gmail.com>
2016-12-14 16:32:23 -0200

    Updated simulation file

--- default.py
+++ default.py
@@ -1,64 +1,42 @@
 from morse.builder import *
 from scripts import const
 
-simMapVisit1 = []
-for i in range(const.MAP_HEIGHT * const.MAP_WIDTH):
-    simMapVisit1.append(-1)
+#Defining robots
+names = ["robot1", "robot2"]
 
-simMapVisit2 = []
-for i in range(const.MAP_HEIGHT * const.MAP_WIDTH):
-    simMapVisit2.append(-1)
+for i in range(len(names)):
+    robot = ATRV(names[i])
 
+    #Defining components
+    mapVisit = []
+    for j in range(const.MAP_HEIGHT * const.MAP_WIDTH):
+        mapVisit.append(-1)
 
-robot1 = ATRV()
-robot2 = ATRV()
+    motion = MotionVW()
 
-motion1 = MotionVW()
-motion2 = MotionVW()
+    sick = Sick()
+    sick.properties(laser_range = 10)
+    sick.properties(scan_window = 180)
+    sick.properties(resolution = 0.5)
+    sick.properties(Visible_arc = True)
 
-robot1.append(motion1)
-robot2.append(motion2)
+    pose = Pose()
+    pose.translate(z = -0.10)
 
-setattr(robot1, "visitMap", simMapVisit1)
-setattr(robot2, "visitMap", simMapVisit2)
+    robot.append(sick)
+    robot.append(motion)
+    robot.append(pose)
+    setattr(robot, "visitMap", mapVisit)
 
-robot1.translate(x = 0, y = 0)
-robot2.translate(x = -3, y = 0)
-robot2.rotate(z = 3.1415)
+    sick.add_interface('socket')
+    motion.add_interface('socket')
+    pose.add_interface('socket')
 
-sick1 = Sick()
-sick1.properties(laser_range = 10)
-sick1.properties(scan_window = 180)
-sick1.properties(resolution = 0.5)
-sick1.properties(Visible_arc = True)
-robot1.append(sick1)
+    if (names[i] == "robot2"):
+        robot.translate(x = -3, y = 0)
+        robot.rotate(z = 3.1415)
 
-sick2 = Sick()
-sick2.properties(laser_range = 10)
-sick2.properties(scan_window = 180)
-sick2.properties(resolution = 0.5)
-sick2.properties(Visible_arc = True)
-robot2.append(sick2)
-
-pose1 = Pose()
-pose1.translate(z = -0.10)
-pose1.add_stream('socket')
-robot1.append(pose1)
-
-pose2 = Pose()
-pose2.translate(z = -0.10)
-pose2.add_stream('socket')
-robot2.append(pose2)
-
-mov = Keyboard()
-mov.properties(Speed = 5)
-robot1.append(mov)
-
-pose1.add_interface('socket')
-sick1.add_interface('socket')
-motion1.add_interface('socket')
-
+#Set enviroment
 env = Environment('indoors-1/boxes')
 env.set_camera_location([0, 0, 60])
 env.set_camera_rotation([0, 0, 0])
-#env.select_display_camera(camera)
